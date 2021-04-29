620fc1d6ab280d29688b482a5731841f42ffecc1
niuBi <1647241302@qq.com>
2021-04-29 14:29:02 +0800

    你好

---
+++ nihao.js
@@ -0,0 +1,12 @@
+console.log('nihao')
+function na() {
+    console.log('niliangle ')
+    if (true) {
+        let a = 6;
+        a = 8 + 9;
+    }
+}
+console.log('杀死比尔')
+console.log('桑啊')
+let l=7;
+let l=8;
